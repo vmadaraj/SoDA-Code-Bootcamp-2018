64641ba200ddfe6d68de28b03acfe9a67c35495c
vmadaraj <35466719+vmadaraj@users.noreply.github.com>
2018-10-14 15:54:24 -0700

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,7 +2,7 @@ let baseApiUrl;
 if (process.env.NODE_ENV === "development") {
   baseApiUrl = "http://127.0.0.1:8000";
 } else {
-  baseApiUrl = "http://sodabootcamp-env.mnmqvzedmy.us-east-1.elasticbeanstalk.com";
+  baseApiUrl = "http://vmadaraj-env.izvnnyqpcf.us-east-1.elasticbeanstalk.com/";
 }
 
 export {
